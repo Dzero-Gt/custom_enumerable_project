fb6e23c05d676d87cc74bd68d03e6fab64fc84e0
Dzero-GT <dillongustafson2002@gmail.com>
2022-06-01 16:10:53 -0500

    basic enumerables created FOR ARRAYS ONLY!

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,95 @@
 module Enumerable
   # Your code goes here
+  def my_each
+    return self.to_enum unless block_given?
+    if self.is_a?(Array)
+      self.size.times {|index| yield self[index]}
+      self
+    else
+      self.keys.size.times {|key_index| yield self.keys[key_index], self[self.keys[key_index]]} #applies for hash?
+      self
+    end
+  end
+
+  def my_each_with_index
+    return self.to_enum unless block_given?
+      if self.is_a?(Array)
+        self.size.times {|index| yield self[index], index}
+        self
+      end
+  end
+
+  def my_select
+    return self.to_enum unless block_given?
+    output = self.is_a?(Hash) ? {} : []
+    self.my_each do |ev|
+      if yield ev
+        self.is_a?(Hash) ? output.store(ev[0], ev[1]) : output << ev
+      end
+    end
+    return output
+  end
+
+  def my_all?
+    return true unless block_given?
+    same = true
+    self.my_each do |ev|
+      if yield ev
+      else
+        same = false
+      end
+    end
+    return same
+  end
+
+  def my_any?
+    return true unless block_given?
+    same = false
+    self.my_each do |ev|
+      if yield ev
+        same = true
+      end
+    end
+    return same
+  end
+
+  def my_none?
+    return false unless block_given?
+    unique = true
+    self.my_each do |ev|
+      if yield ev
+        unique = false
+      end
+    end
+    return unique
+  end
+
+  def my_count
+    return self.length unless block_given?
+    counter = 0
+    self.my_each do |ev|
+      if yield ev
+        counter += 1
+      end
+    end
+    return counter
+  end
+
+  def my_map
+    return self.to_enum unless block_given?
+    output = []
+    my_each { |ev| output<< yield(ev)}
+    return output
+  end
+
+  def my_inject(initial_value = 0, &block)
+    leng = self.length
+    for i in 0..(leng - 1)
+      initial_value = yield(initial_value, self[i])
+    end
+    return initial_value
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +98,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  include Enumerable
+  def initialize(arr)
+    @arr = arr
+  end
 end
+
